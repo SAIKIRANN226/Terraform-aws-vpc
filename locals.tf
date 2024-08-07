9fe861fbec0bc015ebb7d6b80399cffe0289b213
mothukuru saikiran <obscuresai.com>
2024-08-07 20:07:58 +0530

    message

--- locals.tf
+++ locals.tf
@@ -2,3 +2,6 @@ locals {
   name = "${var.project_name}-${var.environment}"
   az_names = slice(data.aws_availability_zones.azs.names,0,2)
 }
+
+# i will fetch az from aws using data source and i will get first 2 az
+# data source to fetch az in aws--> go through the data.tf
